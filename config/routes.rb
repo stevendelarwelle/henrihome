412cf98a4f8353189dab627faf123d006a521d96
Steve <sdelarwelle@wspackaging.com>
2021-09-29 16:36:01 -0500

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'services/get_quote'
+  get 'services/rate_quote'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
+
 end

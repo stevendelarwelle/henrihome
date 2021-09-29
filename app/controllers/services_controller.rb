f158434163bfd6c62e9694b06737a35e331f719d
Steve <sdelarwelle@wspackaging.com>
2021-09-29 18:12:23 -0500

    updated to create object with instance variables.

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,11 +1,12 @@
 class ServicesController < ApplicationController
 
   def get_quote
-    swanson_quote = SwansonQuoteService.get_quote
-    puts swanson_quote.quote_ratings.did_rate?(request.remote_ip)
-    if swanson_quote.nil?
+    swanson_quote_service = SwansonQuoteService.new
+
+    if swanson_quote_service.quote.nil?
       render :status => 400
     else
+      swanson_quote = SwansonQuote.find_or_create_by(quote: swanson_quote_service.quote)
       render :json => {quote: swanson_quote.quote, rating: swanson_quote.quote_ratings.get_avg, did_rate: swanson_quote.quote_ratings.did_rate?(request.remote_ip)}
     end
   end
--- app/models/swanson_quote_service.rb
+++ app/models/swanson_quote_service.rb
@@ -1,14 +1,15 @@
 class SwansonQuoteService
   include HTTParty
-
-  def self.get_quote
+  def initialize
+    @quote = get_quote
+  end
+  def get_quote
       begin
         #we get a random quote from ron-swanson_quotes
         response = HTTParty.get('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
-        #quote_str = response.body.gsub('[', '').gsub(']', '').gsub('"', '')
-        quote = JSON.parse(response.body)[0]
-        swanson_quote = SwansonQuote.find_or_create_by(quote: quote)
-        return swanson_quote
+
+        return JSON.parse(response.body)[0]
+
       rescue HTTParty::Error
         return nil
         # don´t do anything we handle this in the controller
@@ -16,5 +17,8 @@ class SwansonQuoteService
         return nil
         # don´t do anything we handle this in the controller
       end
+  end
+  def quote
+    @quote
   end
 end
